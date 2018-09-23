bf25dbc8384f982e7c41433ffe0d5dc9f0f8068e
Adam Smakulski <adam.smakulski@gmail.com>
2018-09-23 09:24:53 +0200

    e02-fibonacci

--- Exercises/E02-fibonacci/src/com/infoshareacademy/Fibonacci.java
+++ Exercises/E02-fibonacci/src/com/infoshareacademy/Fibonacci.java
@@ -6,6 +6,12 @@ package com.infoshareacademy;
 public class Fibonacci {
 
     private long fib(long n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
         return fib(n - 1) + fib(n - 2);
     }
 
